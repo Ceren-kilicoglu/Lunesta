498a7721d1389cce4e9875164828f27e117533d7
Ceren-kilicoglu <cern.yaa@gmail.com>
2025-01-16 17:38:13 +0300

    setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        spicy: ['"Spicy Rice"', 'serif'],
+        inter: ['Inter', 'sans-serif']
+      },
+      colors: {
+        "white": "#FFFFFF",
+        "blue": "#23A6F0",
+        "navcolor": "#737373",
+        "brand": "#252B42",
+        "darkb": "#14213D"
+      },
+    },
   },
   plugins: [],
 }
-
