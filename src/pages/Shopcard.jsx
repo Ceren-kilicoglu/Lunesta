bafb74cf653fd873b5fed5885f5f48725ecaa16a
Ceren-kilicoglu <cern.yaa@gmail.com>
2025-01-22 12:51:35 +0300

    update code and css

--- src/pages/Shopcard.jsx
+++ src/pages/Shopcard.jsx
@@ -3,34 +3,75 @@ import { data } from '../data/data';
 
 function ShopCard() {
     return (
-        <div className='bg-gwhit h-max-content p-8'>
+        <div className='bg-gwhit h-max-content p-8 '>
             <div className='flex flex-col items-center gap-3 mt-12'>
                 <h3 className='text-3xl text-brand font-bold'>{data.shopcard[0].title}</h3>
                 <p className='text-stext text-base w-[193px] text-center'>{data.shopcard[0].text}</p>
             </div>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-8">
-                {data.shopcard.slice(1).map((item, index) => (
-                    <div
-                        key={item.id}
-                        className={`relative ${index === 0 ? 'lg:col-span-2 lg:row-span-2' :
-                            index === 1 ? 'lg:col-span-2' : ''
-                            }`}
+            <div className="flex flex-col flex-grow sm:flex-row sm:gap-x-4  mt-12 w-max-screen sm:flex-wrap justify-center">
+                {/* İlk öğe: WOMAN */}
+                <div className="">
+                    <img
+                        src={data.shopcard[1].img}
+                        alt={data.shopcard[1].buttonText}
+                        className="lg:w-[510px] lg:h-[520px] object-cover"
+                    />
+                    <button
+                        onClick={() => window.location.href = data.shopcard[1].buttonLink}
+                        className="relative bottom-14 left-8 text-black bg-gwhit hover:bg-pinky hover:text-white  font-medium  text-sm px-5 py-2.5  "
                     >
+                        {data.shopcard[1].buttonText}
+                    </button>
+                </div>
+
+                {/* İkinci öğe: MAN */}
+                <div className="">
+                    <img
+                        src={data.shopcard[2].img}
+                        alt={data.shopcard[2].buttonText}
+                        className="lg:w-[239px] lg:h-[520px] object-cover"
+                    />
+                    <button
+                        onClick={() => window.location.href = data.shopcard[2].buttonLink}
+                        className="relative bottom-14 left-8 text-black bg-gwhit hover:bg-pinky hover:text-white  font-medium  text-sm px-5 py-2.5 "
+                    >
+                        {data.shopcard[2].buttonText}
+                    </button>
+                </div>
+                <div className='md:flex lg:flex-col md:gap-x-4'>
+                    {/* Üçüncü öğe: WOMAN */}
+                    <div className="">
                         <img
-                            src={item.img}
-                            alt={item.buttonText}
-                            className="w-full h-full object-cover rounded-lg"
+                            src={data.shopcard[3].img}
+                            alt={data.shopcard[3].buttonText}
+                            className=" lg:w-[239px] lg:h-[240px] object-cover"
                         />
                         <button
-                            onClick={() => window.location.href = item.buttonLink}
-                            className="absolute bottom-4 left-1/2 transform -translate-x-1/2 bg-gwhit font-semibold text-brand px-4 py-2 rounded-sm shadow-md hover:bg-pinky hover:text-white transition-all duration-300"
+                            onClick={() => window.location.href = data.shopcard[3].buttonLink}
+                            className="relative bottom-14 left-8 text-black bg-gwhit hover:bg-pinky hover:text-white  font-medium  text-sm px-5 py-2.5 "
                         >
-                            {item.buttonText}
+                            {data.shopcard[3].buttonText}
                         </button>
                     </div>
-                ))}
+
+                    {/* Dördüncü öğe: MAN */}
+                    <div className="">
+                        <img
+                            src={data.shopcard[4].img}
+                            alt={data.shopcard[4].buttonText}
+                            className="lg:w-[239px] lg:h-[240px] object-cover"
+                        />
+                        <button
+                            onClick={() => window.location.href = data.shopcard[4].buttonLink}
+                            className="relative bottom-14 left-8 text-black bg-gwhit hover:bg-pinky hover:text-white  font-medium  text-sm px-5 py-2.5 "
+                        >
+                            {data.shopcard[4].buttonText}
+                        </button>
+                    </div>
+                </div>
             </div>
+
         </div>
     );
 }
